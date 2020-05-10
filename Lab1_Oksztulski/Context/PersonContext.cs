6dfe1c45d926d6572cdc4f0593c737fc9f6dda4e
koksztulski <koksztulski@gmail.com>
2020-05-10 19:29:11 +0200

    first message

---
+++ Lab1_Oksztulski/Context/PersonContext.cs
@@ -0,0 +1,21 @@
+﻿using Lab1_Oksztulski.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab1_Oksztulski.Context
+{
+    public class PersonContext :DbContext
+    {
+       
+        private readonly string _connectionString = "Server=tcp:koksztulski-cdv.database.windows.net,1433;Initial Catalog=koksztulski_cdv;Persist Security Info=False;User ID=koksztulski;Password=iksLut25KoLim@k;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        public DbSet<Person> People { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+    }
+}
