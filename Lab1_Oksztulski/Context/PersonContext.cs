16530672c25002aae5e8aa45445f64243e55ac02
koksztul <56515118+koksztul@users.noreply.github.com>
2020-05-20 12:41:29 +0200

    Update PersonContext.cs

--- Lab1_Oksztulski/Context/PersonContext.cs
+++ Lab1_Oksztulski/Context/PersonContext.cs
@@ -10,7 +10,7 @@ namespace Lab1_Oksztulski.Context
     public class PersonContext :DbContext
     {
        
-        private readonly string _connectionString = "Server=tcp:koksztulski-cdv.database.windows.net,1433;Initial Catalog=koksztulski_cdv;Persist Security Info=False;User ID=koksztulski;Password=iksLut25KoLim@k;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        private readonly string _connectionString = "Server=tcp:koksztulski-cdv.database.windows.net,1433;Initial Catalog=koksztulski_cdv;Persist Security Info=False;User ID=koksztulski;Password=;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
         public DbSet<Person> People { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
